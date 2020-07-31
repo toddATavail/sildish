13eb994d59d4679cbb5991ef12e73a47c7a7a537
Todd L. Smith <todd@availlang.org>
2020-07-30 19:43:59 -0500

    Initial Commit

---
+++ sildish/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sildish
+//
+//  Created by Todd L Smith on 7/30/20.
+//  Copyright © 2020 Todd L Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
